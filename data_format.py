5fd1988588a3b9efd99b6bdbbd370bd65e6499fd
Franky170504 <laukiksuryavanshi17@gmail.com>
2025-02-07 19:23:22 +0530

    csv-integration

---
+++ data_format.py
@@ -0,0 +1,17 @@
+import xarray as xr
+
+# Load NetCDF file
+ds_temp = xr.open_dataset("weather-nc\pune_temp.nc")
+ds_preci = xr.open_dataset("weather-nc\pune_preci.nc")
+
+# Convert to Pandas DataFrame
+df_temp = ds_temp.to_dataframe().reset_index()
+df_preci = ds_preci.to_dataframe().reset_index()
+
+# Save as CSV
+df_temp.to_csv("artifacts\pune_weather_temperature.csv", index=False)
+df_preci.to_csv("artifacts\pune_weather_precipitation.csv", index=False)
+
+print(df_temp.head())
+print(df_preci.head())
+
