44c9624cc93eba24e439b99ac458a07fcde17033
Franky170504 <laukiksuryavanshi17@gmail.com>
2025-02-25 00:26:39 +0530

    final dataset

--- file_conversion.py
+++ file_conversion.py
@@ -1,64 +1,47 @@
 import xarray as xr
 import os
 
-ds_temp = xr.open_dataset("weather-nc\_temperature.nc")
-ds_vol_soil_water_l1 = xr.open_dataset("weather-nc\_volumetric_soil_water_layer_1.nc")
-ds_preci = xr.open_dataset("weather-nc\precipitation.nc")
-ds_sea_surface_temp = xr.open_dataset("weather-nc\sea_surface_temperature.nc")
-ds_soil_temp_l1 = xr.open_dataset("weather-nc\soil_temperature_level_1.nc")
-ds_soil_type = xr.open_dataset("weather-nc\soil_type.nc")
-ds_surface_pressure = xr.open_dataset("weather-nc\surface_pressure.nc")
+ds_max_temp = xr.open_dataset("weather-nc\max_temperature.nc")
+ds_mean_temp = xr.open_dataset("weather-nc\mean_temperature.nc")
+ds_max_preci = xr.open_dataset("weather-nc\max_precipitation.nc")
+ds_mean_preci = xr.open_dataset("weather-nc\mean_precipitation.nc")
+ds_mslp = xr.open_dataset("weather-nc\mslp.nc")
 
 # Convert to Pandas DataFrame
-df_temp = ds_temp.to_dataframe().reset_index()
-df_vol_soil_water_l1 = ds_vol_soil_water_l1.to_dataframe().reset_index()
-df_preci = ds_preci.to_dataframe().reset_index()
-df_sea_surface_temp = ds_sea_surface_temp.to_dataframe().reset_index()
-df_soil_temp_l1 = ds_soil_temp_l1.to_dataframe().reset_index()
-df_soil_type = ds_soil_type.to_dataframe().reset_index()
-df_surface_pressure = ds_surface_pressure.to_dataframe().reset_index()
-
+df_max_temp = ds_max_temp.to_dataframe().reset_index()
+df_mean_temp = ds_mean_temp.to_dataframe().reset_index()
+df_max_preci = ds_max_preci.to_dataframe().reset_index()
+df_mean_preci = ds_mean_preci.to_dataframe().reset_index()
+df_mslp = ds_mslp.to_dataframe().reset_index()
 
 # Save as CSV
-if os.path.exists("notebook\data\_temperature.csv"):
-    print("Tempreture File already exists")
+if os.path.exists("notebook\data\max_temperature.csv"):
+    print("Max Tempreture File already exists")
 else:
-    df_temp.to_csv("notebook\data\_temperature.csv", index= False)
-    print("Tempreture File created")
+    df_max_temp.to_csv("notebook\data\max_temperature.csv", index= False)
+    print("Max Tempreture File created")
 
-if os.path.exists("notebook\data\_volumetric_soil_water_layer_1.csv"):
-    print("Soil Water Layer 1 File already exists")
+if os.path.exists("notebook\data\mean_temperature.csv"):
+    print("Mean Tempreture File already exists")
 else:
-    df_vol_soil_water_l1.to_csv("notebook\data\_volumetric_soil_water_layer_1.csv", index= False)
-    print("Soil Water Layer 1 File created")
+    df_mean_temp.to_csv("notebook\data\mean_temperature.csv", index= False)
+    print("Mean Tempreture File created")
 
-if os.path.exists("notebook\data\precipitation.csv"):
-    print("Precipitation File already exists")
+if os.path.exists("notebook\data\max_precipitation.csv"):
+    print("Max Precipitation File already exists")
 else:
-    df_preci.to_csv("notebook\data\precipitation.csv", index=False)
-    print("Precipitation File created")
+    df_max_preci.to_csv("notebook\data\max_precipitation.csv", index=False)
+    print("Max Precipitation File created")
 
-if os.path.exists("notebook\data\sea_surface_temperature.csv"):
-    print("Sea Surface Temperature File already exists")
+if os.path.exists("notebook\data\mean_precipitation.csv"):
+    print("Mean Precipitation File already exists")
 else:
-    df_sea_surface_temp.to_csv("notebook\data\sea_surface_temperature.csv", index=False)
-    print("Sea Surface Temperature File created")
+    df_mean_preci.to_csv("notebook\data\mean_precipitation.csv", index=False)
+    print("Mean Precipitation File created")
 
-if os.path.exists("notebook\data\soil_temperature_level_1.csv"):
-    print("Soil Temperature Level 1 File already exists")
-else:
-    df_soil_temp_l1.to_csv("notebook\data\soil_temperature_level_1.csv", index=False)
-    print("Soil Temperature Level 1 File created")
-
-if os.path.exists("notebook\data\soil_type.csv"):
-    print("Soil Type File already exists")
-else:
-    df_soil_type.to_csv("notebook\data\soil_type.csv", index=False)
-    print("Soil Type File created")
-
-if os.path.exists("notebook\data\surface_pressure.csv"):
-    print("Surface Pressure File already exists")
-else:
-    df_surface_pressure.to_csv("notebook\data\surface_pressure.csv", index=False)
-    print("Surface Pressure File created")
+if os.path.exists("notebook\data\mslp.csv"):
+    print("Mslp File already exists")
+else:   
+    df_mslp.to_csv("notebook\data\mslp.csv", index=False)
+    print("Mslp File created")
 
